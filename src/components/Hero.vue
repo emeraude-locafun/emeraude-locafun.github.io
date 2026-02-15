ff758dec84bf64f4abd5eb838fe100defbb897ba
Olivier Barais <olivieR.barais@gmail.com>
2026-02-15 21:05:29 +0100

    update text

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -60,7 +60,7 @@
 
         <!-- Description (Slot pour le texte riche ou HTML) -->
       <div class="text-lg md:text-xl text-back-100 max-w-2xl mx-auto leading-relaxed opacity-90 prose prose-emerald">
-        <div class=" m-3">
+        <div class=" mb-16 animate-fade-in">
         <slot />
         </div>
       </div>
