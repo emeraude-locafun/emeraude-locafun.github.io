caff94886cf41fcd163212941f76b08e8bb4e9cb
Olivier Barais <olivieR.barais@gmail.com>
2026-02-16 10:43:47 +0100

    add a page for meteo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -146,6 +146,7 @@ const isScrolled = ref(false);
 const navItems = [
   { name: 'Accueil', href: '/#' },
   { name: 'Actualités', href: '/actualites' },
+  { name: 'Météo', href: '/meteo' },
   { name: 'La Flotte', href: '/#fleet' },
   { name: 'Autres prestations', href: '/#prestations' },
   { name: 'Tarifs', href: '/#pricing' }
